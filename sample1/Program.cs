cb04d9e8d58d6bcb31e2bf55bf3f40ae050162a5
Salma MAAZOUM <134633040+Salmamaazoum@users.noreply.github.com>
2026-03-30 07:48:07 +0000

    exo1

---
+++ sample1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace sample1;
+
+public class Personne
+{
+    public required string nom { get; set; }
+    public required int age { get; set; }
+
+    public string Hello(bool isLowercase)
+    {
+        string message = $"hello {nom}, you are {age}";
+
+        if (isLowercase)
+            return message;
+        else
+            return message.ToUpper();
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Personne p = new Personne
+        {
+            nom = "Alice",
+            age = 25
+        };
+
+        Console.WriteLine(p.Hello(true));
+    }
+}
