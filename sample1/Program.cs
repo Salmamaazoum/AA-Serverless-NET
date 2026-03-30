991ecc445ecc4a28c1d0e9e6dc102cad218761c1
Salma MAAZOUM <134633040+Salmamaazoum@users.noreply.github.com>
2026-03-30 08:53:52 +0000

    exo3

--- sample1/Program.cs
+++ sample1/Program.cs
@@ -1,6 +1,8 @@
 ﻿namespace sample1;
 using Newtonsoft.Json;
-
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using System.Diagnostics;
 public class Personne
 {
     public required string nom { get; set; }
@@ -29,5 +31,29 @@ class Program
         string json = JsonConvert.SerializeObject(p, Formatting.Indented);
 
         Console.WriteLine(json);
+
+        string inputFolder = @"imgs";
+        string outputFolder = @"resized-imgs";
+        string[] files = Directory.GetFiles(inputFolder);
+
+        Stopwatch swSeq = Stopwatch.StartNew();
+        foreach (string file in files)
+        {
+            using Image image = Image.Load(file);
+            image.Mutate(x => x.Resize(image.Width / 2, image.Height / 2));
+            image.Save(Path.Combine(outputFolder, "resized_seq" + Path.GetFileName(file)));
+        }
+        swSeq.Stop();
+        Console.WriteLine($"Processing the image seq {swSeq.ElapsedMilliseconds} ms");
+
+        Stopwatch swPara = Stopwatch.StartNew();
+        Parallel.ForEach(files, file =>
+        {
+            using Image image = Image.Load(file);
+            image.Mutate(x => x.Resize(image.Width / 2, image.Height / 2));
+            image.Save(Path.Combine(outputFolder, "resized_par" + Path.GetFileName(file)));
+        });
+        swPara.Stop();
+        Console.WriteLine($"Processing time parallel: {swPara.ElapsedMilliseconds} ms");
     }
 }
--- sample1/obj/Debug/net10.0/sample1.AssemblyInfo.cs
+++ sample1/obj/Debug/net10.0/sample1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sample1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18be6ad0f6e1b2c324520990bd343b7943545407")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59e974d798eb0a7341eee808ed3b80ca1650e111")]
 [assembly: System.Reflection.AssemblyProductAttribute("sample1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sample1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
